842f8c546e7c59df76095f72dec41f46375fe117
Chaokobo <91595312+Chaokobo@users.noreply.github.com>
2024-06-05 15:52:30 +0200

    Update 2024_BDD_BOULANGER_SAM_EX7.sql

--- scripts/2024_BDD_BOULANGER_SAM_EX7.sql
+++ scripts/2024_BDD_BOULANGER_SAM_EX7.sql
@@ -1,3 +1,15 @@
+/* UPDATEs pour l'exo 8 */
+UPDATE etudiants SET nbReservations = 5 WHERE numeroEtudiant = 22311237;
+UPDATE etudiants SET nbReservations = 1 WHERE numeroEtudiant = 22002570;
+UPDATE etudiants SET nbReservations = 1 WHERE numeroEtudiant = 22310344;
+UPDATE materiel SET nbReservationsMat = 2 WHERE idMateriel = 1;
+UPDATE materiel SET nbReservationsMat = 2 WHERE idMateriel = 3;
+UPDATE materiel SET nbReservationsMat = 1 WHERE idMateriel = 7;
+UPDATE materiel SET nbReservationsMat = 1 WHERE idMateriel = 8;
+UPDATE materiel SET nbReservationsMat = 1 WHERE idMateriel = 9;
+
+
+/* exo 7 */
 INSERT INTO reservation (dateDébut, dateFin, numeroEtudiant, idMateriel) VALUES ("2023-10-23", "2023-11-27", 22002570, 1), ("2023-09-23", "2023-12-27", 22311237, 1);
 SELECT nom, prenom FROM etudiants INNER JOIN reservation ON reservation.numeroEtudiant = etudiants.numeroEtudiant GROUP BY nom;
 SELECT idMateriel, description FROM materiel WHERE idMateriel NOT IN (SELECT idMateriel FROM reservation);
